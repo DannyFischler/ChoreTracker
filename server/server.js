6b9e0f7858fc6ca10a9a70a8f281bc2c66df7bb2
Danny <danzfischler@gmail.com>
2023-12-29 18:36:43 -0500

    added concurrently and reinstalled depenedencies in right folders.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+app.use(cors()); 
 app.use(express.json()); 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/choreTracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Could not connect to MongoDB', err);
-});
+mongoose.connect('mongodb://localhost:27017/choreTracker')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('Could not connect to MongoDB', err);
+  });
+
 
 app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
