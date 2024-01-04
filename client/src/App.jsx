917ef58b2c610facf3d6910611c4cf1cf9841587
Danny <danzfischler@gmail.com>
2024-01-03 19:54:33 -0500

    graphQL basics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ParentRegistration from '../../ChoreTracker/my-react-app/src/components/ParentRegistration';
-import ParentDashboard from '../../ChoreTracker/my-react-app/src/components/ParentDashboard';
-import CreateChildAccount from '../../ChoreTracker/my-react-app/src/components/CreateChildAccount';
-import Login from '../../ChoreTracker/my-react-app/src/components/Login';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient'; // Ensure this path is correct
+import ParentRegistration from './components/ParentRegistration';
+import ParentDashboard from './components/ParentDashboard';
+import CreateChildAccount from './components/CreateChildAccount';
+import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/register" element={<ParentRegistration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<ParentDashboard />} />
-          <Route path="/create-child-account" element={<CreateChildAccount />} />
-          {/* Add additional routes here */}
-        </Routes>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/register" element={<ParentRegistration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<ParentDashboard />} />
+            <Route path="/create-child-account" element={<CreateChildAccount />} />
+            {/* Additional routes can be added here */}
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
+
 export default App;
