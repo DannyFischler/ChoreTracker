5de6b15ec304551fae533ce2787aab5b0e9eb319
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2024-01-07 10:42:00 -0500

    bug fix

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,21 @@
 import { gql } from '@apollo/client';
 
+export const GET_CHORES = gql`
+  query getChores {
+    chores {
+      _id
+      choreId
+      parent_id
+      chore_name
+      amount
+      date_approved
+      date_completed
+      parent_comments
+      child_comments
+    }
+  }
+`;
+
 export const QUERY_ME = gql`
   {
     me {
@@ -19,3 +35,4 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
